dd1243035aa7772ab90c64c041e0611bc384cd67
DScrepter <dlsiderman@gmail.com>
2025-01-16 16:24:11 +0500

    готовый десктоп

--- src/js/app.js
+++ src/js/app.js
@@ -28,16 +28,66 @@ SmoothScroll({
 
 //ванильный js
 
-// document.addEventListener('DOMContentLoaded', () => {
-	
+document.addEventListener('DOMContentLoaded', () => {
+	for (let e of document.querySelectorAll('input[type="range"]')) {
+		const min = e.min == '' ? 0 : e.min;
+		const max = e.max == '' ? 100 : e.max;
+		const container = e.closest('.cost__range');
+		let spanValue = '';
+		if (container) {
+			spanValue = container.querySelector('.value-tip');
+			updateContainer('data-min', min);
+			updateContainer('data-max', max);
+			updateValue();
+		}
+
+		e.style.setProperty('--value', e.value);
+		e.style.setProperty('--min', min);
+		e.style.setProperty('--max', max);
+
+		e.addEventListener('input', () => {
+			updateValue();
+
+		});
+
+		function updateValue() {
+			e.style.setProperty('--value', e.value);
+			if (spanValue) {
+				const containerWidth = e.offsetWidth;
+				const persentage = (e.value / max);
+
+				spanValue.style.left = (persentage * containerWidth) + "px";
+				spanValue.querySelector('span').innerText = e.value;
+			}
+			document.querySelector('.cost__sum .value').innerText = formatNumber(e.value * 2000);
+		}
+		function updateContainer(attr, value) {
+			container.setAttribute(attr, formatNumber(value));
+		}
+
+	}
+
+	const reviewsSlider = new Swiper('.reviews__slider', {
+		loop: true,
+		slidesPerView: 2,
+		watchSlidesProgress: true,
+
+	});
 
 
-// });
+});
 
 //если подключен jq
 
 // $(document).ready(function () {
-	
+
 
 
 // });
+function formatNumber(number) {
+	return new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+		minimumFractionDigits: 0,
+	}).format(number);
+}
